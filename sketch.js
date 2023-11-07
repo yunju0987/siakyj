a7da8104c33a5e4c8b15c35768ca43cc104d81c0
yunju0987 <147472560+yunju0987@users.noreply.github.com>
2023-11-07 19:43:17 +0900

    초기속도, 가속도 변형

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function draw() {
 // A simple Particle class
 let Particle = function(position) {
   this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.velocity = createVector(random(-1, 1), random(-5, -2));
   this.position = position.copy();
   this.lifespan = 255;
 };
